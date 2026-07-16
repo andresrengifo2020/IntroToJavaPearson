6c265f953508ccb9bc4f4506106baab3bb4a15ec
andresrengifo2020 <andres.rengifo2012@gmail.com>
2026-07-16 11:03:38 -0500

    initial commit

---
+++ src/chapter01/Exercise01.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java!");
+
+	}
+
+}
---
+++ src/chapter01/Listing1_2.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+public class Listing1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamental First");
+		System.out.println("Problem Driven");
+		
+
+	}
+
+}
